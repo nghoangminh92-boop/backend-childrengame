836b6ff909636874bae82d52b3d188178f3462f2
hoangminh <nghoangminh92@gmail.com>
2026-07-27 14:58:48 +0900

    2

--- server.js
+++ server.js
@@ -13,12 +13,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "https://childrengame.vercel.app",
+  credentials: true,
+}));
 
 app.use(express.json());
 
